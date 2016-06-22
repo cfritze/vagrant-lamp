91ab1b5d6ac703cde5a94ec0ef4defea9bf5f674
Christian Schmelz <c.fritzkowski@gmail.com>
2016-06-22 11:44:56 +0200

    Add Files

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # vagrant-lamp
+
+Vagrantfile / bootstrap.sh to setup a LAMP stack
+
+
+* setup a Ubuntu 14.04 LTS "Trustry Thar" 64bit box
+
+* make the box accessable by the host at IP `33.33.33.10`
+
+* sync the current folder with `/var/www/html` inside the box
+
+* automatically perform all the commands in bootstrap.sh directly after setting up the box for the first time
+
+The bootstrap.sh will:
+
+* update, upgrade
+
+* create a folder inside /var/www/html
+
+* install apache 2.4, php 5.6 (5.5), MySQL, PHPMyAdmin, git,composer,xdebug, phpunit
+
+* also setting a pre-chosen password for MySQL and PHPMyAdmin
+
+* activate mod_rewrite and add *AllowOverride All* to the vhost settings
+
+You can folder and password inside the bootstrap.sh for sure.
+
+### How to use ?
+
+Put `Vagrantfile` and `bootstrap.sh` inside a folder and do a `vagrant up` on the command line.
+This box uses Ubuntu 14.04 LTS "Trustry Thar" 64bit, so if you don't have the basic box already, do a 
+`vagrant box add ubuntu/trusty64` before.
+
+Set C:\Windows\System32\drivers\etc\host 
+33.33.33.10       test.vm
---
+++ bootstrap.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+# Use single quotes instead of double quotes to make it work with special-character passwords
+PASSWORD='12345678'
+PROJECTFOLDER='myproject'
+
+# create project folder
+sudo mkdir "/var/www/html/${PROJECTFOLDER}"
+
+# set PHP 5.6
+sudo add-apt-repository ppa:ondrej/php5-5.6
+
+# update / upgrade
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# install apache 2.5 and php
+sudo apt-get install -y apache2
+sudo apt-get install -y php5 libapache2-mod-php5 php5-mcrypt
+
+# install mysql and give password to installer
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+sudo apt-get -y install mysql-server
+sudo apt-get install php5-mysql
+
+# install phpmyadmin and give password(s) to installer
+# for simplicity I'm using the same password for mysql and phpmyadmin
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+sudo apt-get -y install phpmyadmin
+
+# setup hosts file
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+    DocumentRoot "/var/www/html/${PROJECTFOLDER}"
+    <Directory "/var/www/html/${PROJECTFOLDER}">
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+)
+echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+
+# enable mod_rewrite
+sudo a2enmod rewrite
+
+# restart apache
+service apache2 restart
+
+# install git
+sudo apt-get -y install git
+
+# install xdebug
+apt-get -y install php5-dev php-pear
+apt-get -y install make
+pecl install xdebug
+xdebugsolocation="$(find / -name 'xdebug.so' 2> /dev/null)"
+echo "
+[xdebug]
+zend_extension=\"$xdebugsolocation\"
+xdebug.default_enable=1
+xdebug.remote_enable=1
+xdebug.remote_handler=dbgp
+xdebug.remote_host=192.168.33.22
+;xdebug.remote_connect_back=1
+xdebug.remote_port=9000
+xdebug.remote_autostart=0
+xdebug.remote_log=/tmp/php5-xdebug.log
+" | sudo tee /etc/php5/apache2/php.ini -a
+
+# restart apache
+service apache2 restart
+
+# install composer
+curl -s https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+# install phpunit
+wget https://phar.phpunit.de/phpunit.phar --referer=https://phar.phpunit.de
+chmod +x phpunit.phar
+sudo mv phpunit.phar /usr/local/bin/phpunit
+
+
